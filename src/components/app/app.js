91cf7543a62ee66583ce65f0ffeda8adebec06b6
Pavlo Mitich <mitich.pavel@gmail.com>
2021-03-16 15:10:05 +0100

    Use small first letter in name of Component for fix previous error. Added event listeners for list-items for adding to favorite. Added example of using style modules and adding SASS files to JS.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,8 +6,19 @@ import PostList from '../post-list';
 import PostAddForm from '../post-add-form';
 
 import './app.css';
-
+//
+// import style from './app.module.css';
+// The file of CSS styles must inclunig ".module.css" in name!!!
+// Use ==> className={style.className} for adding syles from object style.
+//
 const App = () => {
+
+    const data = [
+        {label: 'Going to learn React', important: true, id: 'mjss'},
+        {label: 'This is so good!', important: false, id: 'jdjd'},
+        {label: 'I need a break...', important: false, id: 'poqw'}
+    ];
+
     return (
         <div className="app">
             <AppHeader/>
@@ -15,7 +26,7 @@ const App = () => {
                 <SearchPanel/>
                 <PostStatusFilter/>
             </div>
-            <PostList/>
+            <PostList posts={data}/>
             <PostAddForm/>
         </div> 
     )
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -2,12 +2,25 @@ import React from 'react';
 import PostListItem from '../post-list-item';
 import './post-list.css';
 
-const PostList = () => {
+const PostList = ({posts}) => {
+    const elements = posts.map((item) => {
+        const {id, ...itemProps} = item;
+        return (
+            // eslint-disable-next-line react/jsx-key
+            <li key={id} className='list-group-item'>
+                <PostListItem
+                    // You can write as here or shorter as below
+                    // label={item.label}
+                    // important={item.important}
+                    {...itemProps}
+                />
+            </li>
+        )
+    });
+
     return (
         <ul className="app-list list-group">
-            <PostListItem label="Going to learn React" />
-            <PostListItem label='This is so good!' />
-            <PostListItem label='I need a break...' />
+            {elements}
         </ul>
     )
 }
