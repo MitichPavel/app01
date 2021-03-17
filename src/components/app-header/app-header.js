455fc2e685b092e487512267e5974430356f2f90
Pavlo Mitich <mitich.pavel@gmail.com>
2021-03-17 22:22:42 +0100

    Changed project using "Styled Component" (CSS in JS)

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import './app-header.css';
+import styled from 'styled-components';
 
+const Indicator = styled.div`
+    margin: 0px 0 0 10px;
+    width: 15px;
+    height: 15px;
+    border-radius: 50%;
+    border: 3px solid ${props => props.online ? '#e8d8d8' : '#efefef'};
+    background-color: ${props => props.online ? '#aeaeae' : 'white'};
+`
+const Header = styled.div`
+    display: flex;
+    align-items: flex-end;
+    justify-content: space-between;
+    h1 {
+        font-size: 26px;
+        :hover {
+            color: blue;
+        }
+    }
+    h2 {
+        font-size: 1.2rem;
+        color: grey;
+    }
+`
 const AppHeader = () => {
     return (
-        <div className="app-header d-flex">
-            <h1>Pavlo Mitich</h1>
+        <Header>
+            <div className="d-flex">
+                <h1>Pavlo Mitich</h1>
+                <Indicator online/>
+            </div>
             <h2>5 записей, из них понравилось 0</h2>
-        </div>
+        </Header>
     )
 }
 export default AppHeader;
