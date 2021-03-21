b3753fcac8135f9a43b5e74cab611b5906f22eb8
Pavlo Mitich <mitich.pavel@gmail.com>
2021-03-21 13:34:39 +0100

    Added checking of data (all items of array data must be Objects and not be empty)

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -5,19 +5,29 @@ import './post-list.css';
 
 const PostList = ({posts}) => {
     const elements = posts.map((item) => {
-        const {id, ...itemProps} = item;
-        return (
-            // eslint-disable-next-line react/jsx-key
-            <ListGroupItem key={id}>
-                <PostListItem
-                    // You can write as here or shorter as below
-                    // label={item.label}
-                    // important={item.important}
-                    {...itemProps}
-                />
-            </ListGroupItem>
-        )
+        if(item instanceof Object && item.constructor !== Array && isEmpty(item)) {
+            const {id, ...itemProps} = item;
+            return (
+                // eslint-disable-next-line react/jsx-key
+                <ListGroupItem key={id}>
+                    <PostListItem
+                        // You can write as here or shorter as below
+                        // label={item.label}
+                        // important={item.important}
+                        {...itemProps}
+                    />
+                </ListGroupItem>
+            )
+        }
     });
+
+    function isEmpty(obj) {
+        for(let key in obj)
+        {
+            return true;
+        }
+        return false;
+    }
 
     return (
         <ListGroup className="app-list">
