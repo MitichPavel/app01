97b8ce5945f8aafc876cc6e6011fcb1a2b4e789f
Pavlo Mitich <mitich.pavel@gmail.com>
2021-04-05 20:11:44 +0200

    Added functional for adding new posts

--- src/components/post-add-form/post-add-form.js
+++ src/components/post-add-form/post-add-form.js
@@ -1,22 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Button } from 'reactstrap';
 import './post-add-form.css';
 
-const PostAddForm = ({onAdd}) => {
-    return (
-        <div className="bottom-panel d-flex">
-            <input
-                type="text"
-                placeholder="О чем Вы думаете сейчас"
-                className="form-control new-post-label"
-            />
-            <Button
-                type="submit"
-                outline
-                color="secondary"
-                onClick={() => onAdd('hello')}
-            >Добавить</Button>
-        </div>
-    )
+export default class PostAddForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: ''
+        }
+        this.onValueChange = this.onValueChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onValueChange(e) {
+        this.setState({
+            text: e.target.value
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        if(this.state.text) {
+            this.props.onAdd(this.state.text);
+            this.setState({
+                text: ''
+            });
+        }
+    }
+    render() {
+        return (
+            <form
+                className="bottom-panel d-flex"
+                onSubmit={this.onSubmit}
+            >
+                <input
+                    type="text"
+                    placeholder="О чем Вы думаете сейчас"
+                    className="form-control new-post-label"
+                    onChange={this.onValueChange}
+                    value={this.state.text}
+                />
+                <Button
+                    type="submit"
+                    outline
+                    color="secondary"
+                >Добавить</Button>
+            </form>
+        )
+    }
 }
-export default PostAddForm;
